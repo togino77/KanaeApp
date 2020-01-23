39a31b980e1245b6efce7b09dcb2311cab3c6a98
OGINO Tetsuo <togino77@gmail.com>
2020-01-24 01:57:41 +0900

    [add] ApiManager singleton class

---
+++ KanaeApp/ApiManager.swift
@@ -0,0 +1,16 @@
+//
+//  ApiManager.swift
+//  KanaeApp
+//
+//  Created by OGINO Tetsuo on 2020/01/24.
+//  Copyright © 2020 togino77.gmail.com. All rights reserved.
+//
+
+import Foundation
+
+class ApiManager {
+    static let manager = ApiManager()
+    private init() {
+        
+    }
+}
